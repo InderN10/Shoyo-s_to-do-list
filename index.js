36b674581d4758e863b274b2f5e59df3bb4a0283
InderN10 <midtulah2000@gmail.com>
2024-12-26 12:38:41 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const button= document.getElementsByClassName(".button");
+
+
+
+
+
+
+
+function drawTASK(){
+
+};
+function drawedTASK(){
+
+};
+function editTASK(){
+
+};
+function deleteTASK(){
+
+};
